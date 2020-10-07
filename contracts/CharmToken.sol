a5a4a070bb5abaf7e617043997c93a8d49ef6291
mxwtnb <72396409+mxwtnb@users.noreply.github.com>
2020-10-07 13:25:13 +0100

    Explicitly set governance in charm token instead of using ownable. Add tests

--- contracts/CharmToken.sol
+++ contracts/CharmToken.sol
@@ -2,30 +2,41 @@
 
 pragma solidity ^0.6.12;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
-contract CharmToken is ERC20("Charm", "Charm"), Ownable {
+contract CharmToken is ERC20 {
     using Address for address;
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
+    address public governance;
     mapping(address => bool) public minters;
+
+    constructor() public ERC20("Charm", "Charm") {
+        governance = msg.sender;
+    }
 
     function mint(address account, uint256 amount) public {
         require(minters[msg.sender], "!minter");
         _mint(account, amount);
     }
 
-    function addMinter(address _minter) public onlyOwner {
+    function setGovernance(address _governance) public {
+        require(msg.sender == governance, "!governance");
+        governance = _governance;
+    }
+
+    function addMinter(address _minter) public {
+        require(msg.sender == governance, "!governance");
         minters[_minter] = true;
     }
 
-    function removeMinter(address _minter) public onlyOwner {
+    function removeMinter(address _minter) public {
+        require(msg.sender == governance, "!governance");
         minters[_minter] = false;
     }
 }
