3108b01c86f1d51f82ba3b71da4bbf0f80261b5d
mxwtnb <72396409+mxwtnb@users.noreply.github.com>
2020-10-08 23:07:02 +0100

    Add script to print options json

---
+++ scripts/generate_options.py
@@ -0,0 +1,46 @@
+import datetime
+import json
+
+from brownie import (
+    accounts,
+    Contract,
+    OptionsMarketMaker,
+    OptionsToken,
+)
+
+
+TOKEN_SYMBOLS = {
+    "0x0000000000000000000000000000000000000000": "ETH",
+    "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48": "USDC",
+    "0xED6bfBd086ad6AdaB4A709071f9ae3863796F74A": "MOCK",  # ropsten
+}
+
+
+def main():
+    options = {}
+    for market in OptionsMarketMaker:
+        baseAddress = str(market.baseToken())
+        baseSymbol = TOKEN_SYMBOLS.get(baseAddress, "?")
+
+        addresses = [market.longToken(), market.shortToken()]
+        for i in range(2):
+            address = addresses[i]
+            option = OptionsToken.at(address)
+            symbol = option.symbol()
+            assert symbol.lower() not in options
+            options[symbol.lower()] = {
+                "address": address,
+                "symbol": symbol,
+                "oppositeAddress": addresses[1 - i],
+                "marketAddress": market.address,
+                "oracle": market.oracle(),
+                "alpha": market.alpha(),
+                "expiryTime": market.expiryTime(),
+                "strikePrice": market.strikePrice(),
+                "multiplier": market.multiplier(),
+                "baseAddress": baseAddress,
+                "baseSymbol": baseSymbol,
+                "isLong": i == 0,
+            }
+
+    print(json.dumps(options, indent=4, sort_keys=True))
