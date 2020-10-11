336f304c9fd6e06b64068dde8b87e25d0a1837c4
mxwtnb <72396409+mxwtnb@users.noreply.github.com>
2020-10-11 13:16:47 +0100

    Simplify mock token to not need owner

--- contracts/mocks/MockToken.sol
+++ contracts/mocks/MockToken.sol
@@ -2,19 +2,18 @@
 
 pragma solidity ^0.6.12;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
 
-contract MockToken is ERC20("Mock Token", "MOCK"), Ownable {
+contract MockToken is ERC20("Mock Token", "MOCK") {
     using Address for address;
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
-    function mint(address account, uint256 amount) public onlyOwner {
+    function mint(address account, uint256 amount) public {
         _mint(account, amount);
     }
 }
