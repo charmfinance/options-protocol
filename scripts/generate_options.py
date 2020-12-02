83e5b1647fa36778b3fa26e6d9a3a701809d1550
mxwtnb <72396409+mxwtnb@users.noreply.github.com>
2020-12-02 15:19:41 +0000

    Update generate_options to work for OptionMarket

--- scripts/generate_options.py
+++ scripts/generate_options.py
@@ -1,47 +1,54 @@
 import datetime
 import json
+import sys
 
 from brownie import (
     accounts,
     Contract,
-    OptionsMarketMaker,
-    OptionsToken,
+    OptionMarket,
+    OptionToken,
 )
 
 
 TOKEN_SYMBOLS = {
     "0x0000000000000000000000000000000000000000": "ETH",
     "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48": "USDC",
-    "0xED6bfBd086ad6AdaB4A709071f9ae3863796F74A": "MOCK",  # ropsten
+    "0xE7d541c18D6aDb863F4C570065c57b75a53a64d3": "USDC",  # rinkeby
 }
 
 
 def main():
-    options = []
-    for market in OptionsMarketMaker:
+    with open("markets.json", "r") as f:
+        markets = json.load(f)
+
+    res = []
+    for address in markets:
+        market = OptionMarket.at(address)
         baseAddress = str(market.baseToken())
-        baseSymbol = TOKEN_SYMBOLS.get(baseAddress, "?")
+        baseSymbol = TOKEN_SYMBOLS.get(baseAddress, "")
 
-        addresses = [market.longToken(), market.shortToken()]
-        for i in range(2):
-            address = addresses[i]
-            option = OptionsToken.at(address)
-            symbol = option.symbol()
-            options.append(
-                {
-                    "address": address,
-                    "symbol": symbol,
-                    "oppositeAddress": addresses[1 - i],
-                    "marketAddress": market.address,
-                    "oracle": market.oracle(),
-                    "isPutMarket": market.isPutMarket(),
-                    "alpha": market.alpha(),
-                    "expiryTime": market.expiryTime(),
-                    "strikePrice": market.strikePrice(),
-                    "baseAddress": baseAddress,
-                    "baseSymbol": baseSymbol,
-                    "isLong": i == 0,
-                }
-            )
+        n = market.numStrikes()
+        longTokens = [OptionToken.at(market.longTokens(i)) for i in range(n)]
+        shortTokens = [OptionToken.at(market.shortTokens(i)) for i in range(n)]
 
-    print(json.dumps(options, indent=4, sort_keys=True))
+        res.append(
+            {
+                "address": address,
+                "baseAddress": baseAddress,
+                "baseSymbol": baseSymbol,
+                "oracleAddress": market.oracle(),
+                "expiryTime": market.expiryTime(),
+                "alpha": market.alpha(),
+                "isPut": market.isPut(),
+                "tradingFee": market.tradingFee(),
+                "balanceCap": market.balanceCap(),
+                "totalSupplyCap": market.totalSupplyCap(),
+                "strikePrices": [market.strikePrices(i) for i in range(n)],
+                "longAddresses": [token.address for token in longTokens],
+                "longSymbols": [token.symbol() for token in longTokens],
+                "shortAddresses": [token.address for token in shortTokens],
+                "shortSymbols": [token.symbol() for token in shortTokens],
+            }
+        )
+
+    print(json.dumps(res, indent=4, sort_keys=True))
