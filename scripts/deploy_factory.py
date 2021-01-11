1dc8ff38015432a40c9c43100c7fb1b515c883d1
mxwtnb <72396409+mxwtnb@users.noreply.github.com>
2021-01-11 18:49:50 +0000

    Set publish_source to true in deploy_factory

--- scripts/deploy_factory.py
+++ scripts/deploy_factory.py
@@ -10,9 +10,11 @@ def main():
     deployer = accounts.load("deployer")
     balance = deployer.balance()
 
-    optionMarket = deployer.deploy(OptionMarket)
-    optionToken = deployer.deploy(OptionToken)
-    factory = deployer.deploy(OptionFactory, optionMarket, optionToken)
+    optionMarket = deployer.deploy(OptionMarket, publish_source=True)
+    optionToken = deployer.deploy(OptionToken, publish_source=True)
+    factory = deployer.deploy(
+        OptionFactory, optionMarket, optionToken, publish_source=True
+    )
 
     print(f"Factory address: {factory.address}")
     print(f"Gas used in deployment: {(balance - deployer.balance()) / 1e18:.4f} ETH")
