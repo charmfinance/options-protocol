bee92c05b631eab4ceb675e0f3e20ea005e21203
mxwtnb <72396409+mxwtnb@users.noreply.github.com>
2021-01-13 16:30:23 +0000

    Remove unused import

--- contracts/OptionMarket.sol
+++ contracts/OptionMarket.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.6.12;
 
-import "@openzeppelin/contracts/math/Math.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
