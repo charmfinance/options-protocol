8d7fa73bd1b9c3b314bd43719f527564038fe6d3
mxwtnb <72396409+mxwtnb@users.noreply.github.com>
2021-01-15 13:12:21 +0000

    Initialize reference contracts with dummy data

--- scripts/deploy_factory.py
+++ scripts/deploy_factory.py
@@ -6,12 +6,31 @@ from brownie import (
 )
 
 
+ZERO = "0x0000000000000000000000000000000000000000"
+
 def main():
     deployer = accounts.load("deployer")
     balance = deployer.balance()
 
     optionMarket = deployer.deploy(OptionMarket, publish_source=True)
     optionToken = deployer.deploy(OptionToken, publish_source=True)
+
+    # initialize with dummy data so can't be initialized again
+    optionToken.initialize(ZERO, "", "", 18)
+    optionMarket.initialize(
+        ZERO,
+        ZERO,
+        [ZERO],
+        [ZERO],
+        [1],
+        2000000000,
+        False,
+        0,
+        0,
+        0,
+        "",
+    )
+
     factory = deployer.deploy(
         OptionFactory, optionMarket, optionToken, publish_source=True
     )
