c2f14bc4a30acffbeedfbe4d04cf2000128cb5c1
mxwtnb <72396409+mxwtnb@users.noreply.github.com>
2021-01-26 18:50:21 +0000

    Add total supply cap and lp symbol to generate_options

--- scripts/generate_options.py
+++ scripts/generate_options.py
@@ -32,16 +32,24 @@ def main():
         longTokens = [OptionToken.at(market.longTokens(i)) for i in range(n)]
         shortTokens = [OptionToken.at(market.shortTokens(i)) for i in range(n)]
 
+        assert market.symbol().startswith("Charm LP ")
+        underlyingSymbol = market.symbol().split()[2]
+
+        totalSupplyCap = (
+            market.totalSupplyCap() if hasattr(market, "totalSupplyCap") else 0
+        )
+
         res.append(
             {
                 "address": address,
                 "baseAddress": baseAddress,
                 "baseSymbol": baseSymbol,
                 "decimals": longTokens[0].decimals(),
-                "underlyingSymbol": longTokens[0].symbol().split()[0],
+                "underlyingSymbol": underlyingSymbol,
                 "oracleAddress": market.oracle(),
                 "expiryTime": market.expiryTime(),
                 "balanceCap": market.balanceCap(),
+                "totalSupplyCap": totalSupplyCap,
                 "isPut": market.isPut(),
                 "tradingFee": market.tradingFee(),
                 "disputePeriod": market.disputePeriod(),
@@ -50,6 +58,7 @@ def main():
                 "longSymbols": [token.symbol() for token in longTokens],
                 "shortAddresses": [token.address for token in shortTokens],
                 "shortSymbols": [token.symbol() for token in shortTokens],
+                "symbol": market.symbol(),
             }
         )
 
