7681248bee46048f69c57648ab9ad442c72ddb1e
mxwtnb <72396409+mxwtnb@users.noreply.github.com>
2021-03-18 15:54:21 +0000

    Modify option registry to automatically add markets from factory. Add inverse lookup methods

--- contracts/periphery/OptionRegistry.sol
+++ contracts/periphery/OptionRegistry.sol
@@ -1,58 +1,92 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.12;
+pragma experimental ABIEncoderV2;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 import "../OptionFactory.sol";
 import "../OptionMarket.sol";
+import "../OptionToken.sol";
 
-contract OptionRegistry is Ownable {
+
+contract OptionRegistry {
     using Address for address;
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
-    mapping(address => mapping(uint256 => mapping(bool => OptionMarket))) markets;
-    mapping(OptionMarket => mapping(uint256 => mapping(bool => OptionToken))) options;
+    struct OptionDetails {
+        bool isLongToken;
+        uint256 strikeIndex;
+        uint256 strikePrice;
+    }
+
+    OptionFactory public constant factory = OptionFactory(0xCDFE169dF3D64E2e43D88794A21048A52C742F2B);
+
+    mapping(IERC20 => mapping(uint256 => mapping(bool => OptionMarket))) public markets; // baseToken => expiry => isPut => market
+    mapping(OptionMarket => mapping(uint256 => mapping(bool => OptionToken))) public options; // market => strikePrice => isLongToken
+    mapping(OptionToken => OptionDetails) public optionDetails;
+
+    uint256 public lastIndex;
 
     /**
-     * @dev Fetch option market from underlying, expiry and whether call/put.
-     * @param underlying Address of underlying ERC20 token. Equal to 0x0 for ETH.
+     * @dev Fetch option market
+     * @param baseToken Address of base token. Same as underlying for calls and
+     * strike currency for puts. Equal to 0x0 for ETH
      * @param expiry Expiry time as timestamp
-     * @param isPut Put if true, call if false
+     * @param isPut True if put, false if call
      */
-    function getMarket(address underlying, uint256 expiry, bool isPut) external view returns (OptionMarket) {
-        return markets[underlying][expiry][isPut];
+    function getMarket(IERC20 baseToken, uint256 expiry, bool isPut) external view returns (OptionMarket) {
+        return markets[baseToken][expiry][isPut];
     }
 
     /**
-     * @dev Fetch option token from market, strike and whether long/short.
-     * @param market Address of {OptionMarket} contract
-     * @param strikePrice Strike price in USD multiplied by 1e18
-     * @param isLong Long position if true, short position if false
+     * @dev Fetch option token
+     * @param market Parent market
+     * @param strikePrice Strike price in USDC multiplied by 1e18
+     * @param isLongToken True if long position, false if short position
      */
-
-    function getOption(OptionMarket market, uint256 strikePrice, bool isLong) external view returns (OptionToken) {
-        return options[market][strikePrice][isLong];
+    function getOption(OptionMarket market, uint256 strikePrice, bool isLongToken) external view returns (OptionToken) {
+        return options[market][strikePrice][isLongToken];
     }
 
     /**
-     * @dev Called by owner to register a market and its options. Should be
-     * called whenever a new market is deployed.
+     * @dev Fetch option details
+     * @param optionToken Option token
      */
-    function registerMarket(OptionMarket market, address underlying) external onlyOwner {
-        markets[underlying][market.expiryTime()][market.isPut()] = market;
+    function getOptionDetails(OptionToken optionToken) external view returns (OptionDetails memory) {
+        return optionDetails[optionToken];
+    }
+
+    /**
+     * @dev Add mappings for any new markets that have been added to factory
+     * since the last time this method was called
+     */
+    function populateMarkets() external {
+        uint256 numMarkets = factory.numMarkets();
+        while (lastIndex < numMarkets) {
+            OptionMarket market = OptionMarket(factory.markets(lastIndex));
+            _populateMarket(market);
+            lastIndex = lastIndex.add(1);
+        }
+    }
+
+    function _populateMarket(OptionMarket market) internal {
+        markets[market.baseToken()][market.expiryTime()][market.isPut()] = market;
 
         uint256 numStrikes = market.numStrikes();
         for (uint256 i = 0; i < numStrikes; i = i.add(1)) {
-            uint256 strike = market.strikePrices(i);
-            options[market][strike][true] = market.longTokens(i);
-            options[market][strike][false] = market.shortTokens(i);
+            OptionToken longToken = market.longTokens(i);
+            OptionToken shortToken = market.shortTokens(i);
+            uint256 strikePrice = market.strikePrices(i);
+
+            options[market][strikePrice][true] = longToken;
+            options[market][strikePrice][false] = shortToken;
+            optionDetails[longToken] = OptionDetails(true, i, strikePrice);
+            optionDetails[shortToken] = OptionDetails(false, i, strikePrice);
         }
     }
 }
